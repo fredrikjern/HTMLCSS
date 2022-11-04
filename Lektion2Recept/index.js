3092caeab2140a8f6620bc23dad086a79fb521cd
Fredrik <fjernberg@gmail.com>
2022-11-04 11:54:28 +0100

    3

---
+++ Lektion2Recept/index.js
@@ -0,0 +1,22 @@
+receptArray = [
+    {
+        name: "Laxpasta",
+        img_url: "https://cdn-rdb.arla.com/Files/arla-se/3185386303/97993690-6b12-4db5-99e5-4a9c327dcf97.jpg?w=1269&h=715&mode=crop&ak=f525e733&hm=b762ca1a",
+        ingredients: {
+            "Pasta":"300 g",
+            "Laxfilé":"400 g",
+            "Creme Fraiche":"2 dl",
+            "Hönsbuljong":"½ Tärning",
+            "Honung":"Lime saft+zest",
+            "Svartpeppar": "1 tsk"
+            },
+        instructions: {
+            1: "Koka pastan enligt anvisning på förpackningen.",
+            2: "Skär laxen i tärningar. Blanda crème fraiche, smulad buljongtärning, honung, skal och saft av lime i en kastrull. Låt såsen koka upp.",
+            3: "Lägg i laxen och låt koka någon minut. Servera med pastan och krydda med peppar."
+            },
+        altUrl: "https://www.arla.se/recept/laxpasta-med-lime/",
+        anecdote: "Liten anekdot om laxpastan weeeee var är min lorem? Detta går ju inte alls som planerat.....",
+        time: "30 min"
+    }
+]
